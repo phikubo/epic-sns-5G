c75e6ceddcef1088b359d811be73df07de08c572
Michael Mart <jmichaelten@gmail.com>
2019-09-26 18:55:09 -0500

    Prototipo inicial Celdas Hexagonales terminado hasta nivel 2.

---
+++ protipo/durbatuluk.py
@@ -0,0 +1,8 @@
+#import
+'''Ash nazg durbatulûk, ash nazg gimbatul'''
+
+if __name__=="__main__":
+	#Prototipo: FUNCION MAIN QUE GOBIERNA TODAS LOS SCRIPTS, FUNCION MAIN QUE BUSCA TODOS LOS SCRIPTS
+	pass
+else:
+	print("Modulo <ring of power> importado")
--- protipo/grid_layout/prot_celda.py
+++ protipo/grid_layout/prot_celda.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import RegularPolygon
 import numpy as np
 import math
-import patron_hexagonal_circular as phc
+import prot_patron_hexagonal_circular as phc
 import time #debug
 #
 def guardar_lista(lista,bd_coordenadas):
@@ -50,7 +50,7 @@ def generar_patron_circular(nivel, patron):
 
 def variables():
 	numero_celdas=1	#requerimiento: funciona como una ventana, un slicing es util para graficar el número deseado
-	nivel=2 #nivel corresponde al nivel de coordenada hexagonal, (1.0.1) nivel 1, (2.0.1) nivel 2, etc.
+	nivel=1 #nivel corresponde al nivel de coordenada hexagonal, (1.0.1) nivel 1, (2.0.1) nivel 2, etc.
 	radio_ext=100/10 #radio de la celda hexagonal, igual en magnitud al lado.
 	apotema=math.sqrt(radio_ext**2 -(0.5*radio_ext)**2) #apotema
 	coordenada_patron=[] #inicialización de coordn
@@ -61,9 +61,15 @@ def test_coordenadas(nivel, coordenada_patron):
 	g=generar_lista(nivel, coordenada_patron)
 
 def distancia_entre_celdas(nivel, radio_ext):
-	'''Funcion que genera el valor de la distancia entre el origen y las celdas por nivel'''
+	'''Funcion que genera el valor de la distancia entre el origen y las celdas por nivel. Puede ser util para calcular el 
+	radio de todo el sistema'''
 	#print(radio_ext)
 	r_med=radio_ext/2
+	
+	#este nivel debe ser constante cuando se llama en el mapeo, porque?
+	#Rta, debido a que la distancia entre celdas, se ve afectada por 2/3*seno(60)(x-z), por esto dec
+	#no debe ser ajustable porque esa distancia se ajusta en la ecuacion de arriba.
+	print("distancia_entre_celdas",3*(nivel)*r_med)
 	#print(r_med, nivel)
 	return 3*(nivel)*r_med
 
@@ -75,25 +81,27 @@ def mapear_coordenadas_cartesianas(coordenadas_axiales, radio_ext):
 	coordenadas_cartesianas_vertical = []
 	for i in range(len(coordenadas_axiales)):
 		print(i)
-	for lista in coordenadas_axiales:
+	for lista, nvl in zip(coordenadas_axiales, range(nivel+1)):
 		print("axiales totales ", lista, "numero de celdas ", len(lista))
-		#time.sleep(0.5)
-		for nvl in range(nivel+1):
-			print("nivel: ",nvl)
-			dec = distancia_entre_celdas(nvl, radio_ext)
-			print("distancia ", dec)
-			for axial in lista:
-				print("coordenada axial ") 
-				print(axial)
-				#time.sleep(0.5)
+		print("nivel: ",nvl)
+		dec = distancia_entre_celdas(1, radio_ext)
+		print("distancia ", dec)
+		print("---------------------")
+
+		for axial in lista:
+			print("coordenada axial ") 
+			print(axial)
+			time.sleep(0.5)
 				
-				coord_y= 2. * np.sin(np.radians(60)) * (dec*axial[1] - dec*axial[2]) /3.
+			coord_y= 2. * np.sin(np.radians(60)) * (dec*axial[1] - dec*axial[2]) /3.
 				
-				print("x final: ", axial[0]*dec )
-				print("y final: ", coord_y )
-				#time.sleep(1)
-				coordenadas_cartesianas_horizontal.append(axial[0]*dec)
-				coordenadas_cartesianas_vertical.append(coord_y)
+			print("x final: ", axial[0]*dec , "---","y final: ", coord_y)
+
+			coordenadas_cartesianas_horizontal.append(axial[0]*dec)
+			coordenadas_cartesianas_vertical.append(coord_y)
+		print("---------------------")
+		print("---------------------")
+		print("---------------------")
 			#		print(coord)
 			#	coordenadas_cartesianas_horizontal.append(coord*dec)
 				#time.sleep(0.4)
@@ -131,13 +139,13 @@ if __name__=="__main__":
 	#coordenada_patron = generar_patron_circular(nivel, coordenada_patron)
 	coordenadas_axiales = phc.ensamblar(nivel)
 	##print(len(coordenadas_axiales))
-	'''Task 2. generar patron en malla'''
-	dec = distancia_entre_celdas(nivel, radio_ext)
+	'''Task 2. generar patron en malla-terminado'''
+	#dec = distancia_entre_celdas(nivel, radio_ext)
 	cartesian_x, cartesian_y = mapear_coordenadas_cartesianas(coordenadas_axiales, radio_ext)
 	print(len(cartesian_x), len(cartesian_y))
 	dibujar_celdas(cartesian_x,cartesian_y, radio_ext)
 	#test_coordenadas(nivel, coordenada_patron)
-	'''Task 3. generar numero de celdas deseadas'''
+	'''Task 3. generar numero de celdas deseadas-hint:slicing'''
 	numero_celdas=3
 	plt.grid(True)
 	plt.savefig("protipo2.png")
