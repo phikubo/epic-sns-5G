9de3243301f572a132a936c584f95a22627d186f
phikubo <jmichaelten@gmail.com>
2020-09-23 21:31:41 -0500

    integracion terminada.

--- prototipo_v038/top_pruebas.py
+++ prototipo_v038/top_pruebas.py
@@ -132,7 +132,7 @@ def prueba_sistema_v047():
 			np.save(f, yy)
 		print("Terminado ok.")
 
-	iteracion=20#
+	iteracion=configuracion["cfg_simulador"]["params_general"]["iteracion"]
 	coleccion_simulacion=[]
 	it=0
 	for n in range(iteracion):
@@ -143,30 +143,13 @@ def prueba_sistema_v047():
 	simtest=coleccion_simulacion[0]
 	simtest.info_sinr()
 	simtest.info_general("general")
-	'''
-	pr=simtest.hiperc_malla_modelo_canal.resultado_balance
 
-	#prmax_v=np.maximum(pr[0],pr[1])
-	#inicializa en la primera matriz, obtiene el maximo.
-	pr_max=pr[0]
-	for ind,pr_i in enumerate(pr):
-		print("indice",ind)
-		#itera sobre las demas.
-		pr_max=np.maximum(pr_max, pr_i)
-	pr_max=pr_max[:-1,:-1]
-	z_min,z_max=-np.abs(pr_max).max(), np.abs(pr_max).max()
-	fig,ax=plt.subplots()
-	xx=simtest.malla_x
-	yy=simtest.malla_y
-	c=ax.pcolormesh(xx,yy,pr_max, cmap='plasma', vmin=z_min, vmax=-40)
-	fig.colorbar(c,ax=ax)
-	plt.grid(True)
-	'''
 	if display_pic:
 		simtest.ver_imagen_potencia()
 		plt.show()
 	else:
 		pass
+
 
 	coleccion_relacion_conexion=[]
 	for simulacion in coleccion_simulacion:
@@ -178,19 +161,19 @@ def prueba_sistema_v047():
 	plt.plot(coleccion_relacion_conexion,'r*-')
 
 	p=1.*np.arange(len(coleccion_relacion_conexion))/(len(coleccion_relacion_conexion)-1)
-
+	print(p[:20])
 	fig=plt.figure()
 	ax1=fig.add_subplot(121)
 	ax1.plot(p,coleccion_relacion_conexion, 'g-')
-	ax1.set_xlabel("$Dist P$")
-	ax1.set_ylabel("$Conexion$")
+	ax1.set_xlabel("$Norm Dist$")
+	ax1.set_ylabel("$Medida SINR$")
 
 	ax2=fig.add_subplot(122)
 	ax2.plot(coleccion_relacion_conexion,p, 'k-')
-	ax2.set_xlabel("$Conexion$")
-	ax2.set_ylabel("$Dist P$")
+	ax2.set_xlabel("$Medida SINR$")
+	ax2.set_ylabel("$Norm Dist$")
 
-
+	
 	plt.show()
 
 
