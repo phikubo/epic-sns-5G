d962807aafffeb9eaf23f70ea78a8e626a891e8d
ftosse <felipeat95gmail.com>
2020-11-23 20:58:02 -0500

    planificador preliminar

--- prototipo_v038/pk_gestion_recursos/planificador.py
+++ prototipo_v038/pk_gestion_recursos/planificador.py
@@ -3,150 +3,111 @@ import numpy as np
 import os
 
 class Planificador:
-	'''Clase: asigna prb por usuario, calcula matriz de interferencia.'''
-	def __init__(self, params_cfg, params_cfg_gen, params):
-		self.cfg_plan=params_cfg
-		self.cfg_gen=params_cfg_gen
+	def __init__(self, params, no_usuarios):
+		self.cfg_plan=params
 		#numero de usuarios por celda
-		self.mapa_conexion=params[0]
-		self.dim_mapa=params[1]
-		self.mapa_usuarios=params[2]
-		self.max_usuario=max(self.mapa_conexion)
-		#self.dim_pot_r=params[1]
-		self.mapa_interf_distribuida=np.ones(params[1])
+		self.no_usuarios=no_usuarios
 		#output:
 		self.asignacion=0 #por usuario
-		self.nrb_total=0
-		self.nrb_total=24
-		self.nrb_con_PBCH=0
-		self.contador=0
-		self.mapa_nrb=[]
-		self.lista_distribucion=[]
-		self.mapa_estado=[]
-		self.mapa_interferencia=[]
-		self.estado=0
-		#self.calcular_tipo_asignacion()
-		self.calcular_nrbs_celda()
-		self.calcular_nrbs_usuarios()
-		self.configurar_mapa_nrb()
-
-
-
-	def to_khz(self,target):
-		'''return variable in khz'''
-		return target*10**3
-
-	def to_mhz(self,target):
-		'''return variable in Mhz'''
-		return target*10**6
-
-	def calcular_nrbs_celda(self):
-		'''Calcula el numero de prbs por cada celda, de acuerdo a numerologia y ancho de banda'''
-		#http://www.techplayon.com/nr-resource-block-definition-and-rbs-calculation/
-		#https://www.rfwireless-world.com/calculators/5G-NR-maximum-throughput-calculator.html
-		#https://5g-tools.com/5g-nr-throughput-calculator/
-		#https://apkpure.com/nr-5g-prb-and-data-rate-calculator/com.instinctcoder.nr5gthecal
-		delta_bw=(2*self.cfg_plan["numerologia"]*self.to_khz(15))
-		#15khz es lo minimo.
-		one_resource_block=self.cfg_plan["sub_ofdm"]*delta_bw
-		nrb_sin_gbw=self.to_mhz(self.cfg_plan["bw"][0])-2*self.to_khz(self.cfg_plan["bw_guarda"][0])
-		nrb=np.floor(nrb_sin_gbw/one_resource_block)
-
-		if self.cfg_plan["pbch"]:
-			self.nrb_con_PBCH=nrb-22
-			self.nrb_total=self.nrb_con_PBCH*self.cfg_plan["trama_total"]
-		else:
-			self.nrb_total=nrb*self.cfg_plan["trama_total"]
-
-
-
-	def calcular_nrbs_usuarios(self):
-		'''Funcion que asigna ancho de banda representado en prb, a partir de la
-		frecuencia portadora y otros parametros adicionales'''
+		self.inicializar_tipo()
+		self.numerologia=[0,1,2,3]
+		self.cp_ofdm=['Normal','Extendido']
+		self.format_slot=['D','U','F']
+		self.case_use=['mMTC','eBMM','URLLC']
+	def inicializar_tipo(self):
 		if self.cfg_plan["tipo"]=="rr":
-			ress=self.nrb_total%self.max_usuario
-			if ress!=0:
-				self.nrb_total=self.nrb_total-ress
-			else:
-				pass
-			self.asignacion=self.nrb_total/self.max_usuario
-
-
-
+			self.asignacion=self.cfg_plan["bw"][0]/self.no_usuarios
 		elif self.cfg_plan["tipo"]=="estatico":
 			self.asignacion=self.cfg_plan["bw"][0]
 		elif self.cfg_plan["tipo"]=="arreglo":
 			#procesa arreglos, gestiona pesos.
-			#this
 			pass
 		elif self.cfg_plan["tipo"]=="futuro":
 			pass
 		else:
 			pass
 
-	def configurar_mapa_nrb(self):
-		'''Reparte recursos C_{i}, de acuerdo al mapa de conexion de celdas.
-		C_{i}, i={1,2,3,...,n_cel}, representa cada bloque de nrb entregado a cada
-		usuario, de esta forma si se entrega a cada usuario 364 nrbs, C1 representa
-		el bloque nrb 0-363, C2 representa el bloque nrb 364-2*363 y asi sucesivamente.'''
-		#print("self.asignacion",self.asignacion)
-		print("mapa", self.mapa_conexion, self.max_usuario)
-		#print("usuarios",self.mapa_usuarios)
-		#el estado debe cambiar solo cuando el indice cambia.
-		self.contador=[0 for i in range(len(self.mapa_conexion))]
-		self.contador=np.array(self.contador)
-		self.estado=[0 for i in range(len(self.mapa_conexion))]
-		self.estado=np.array(self.estado)
 
-		for indd, celda in enumerate(self.mapa_usuarios):
-			self.contador[celda[0]]=self.contador[celda[0]]+1
-			self.estado[celda[0]]=1
-			#cambio de indice.
-			print(indd,self.mapa_usuarios[indd],self.contador, self.estado, "nrb_{}".format(sum(self.contador*self.estado)))
-			self.mapa_estado.append(self.estado.copy())
-			self.mapa_nrb.append(sum(self.contador*self.estado))
-			self.estado[celda[0]]=0
-			#print("nrb",mapa, " repetidos:",arreglo[0])
-		#convertir a numpy
-		self.mapa_nrb=np.array(self.mapa_nrb)
-		self.mapa_estado=np.stack(self.mapa_estado).reshape(self.dim_mapa)
+			
+	def asignar_100mhz():
+		#asiganar ancho de banda en Megaherz y frecuencia portadora  con el fin de asignar la numerologia necesaria.
+		#Se tiene conciencia de que para cumplir con los 264 canales es necesario elevar la frecuencia portadora.
+		#el hecho es tener 264 canales maximos y eso se lleva acabo con tanto el espaciamiento entre portadoras.
+		#y la cantidad de subportadoras  (15KHz*2^numerologia)*12subportadoras = 720KHz Ahora el ancho de banda del sistema 
+		# se divide entre la el ancho del canal, dandonos el numero maxiño de 264 canales, teniendo en cuenta la banda de guarda
+		#
+		#
+		#
+		# canal compartido entre 6OKhHz para hace parte del canal compartido donde se puede apreciar que puede transmitir.
+		# datos con prefijo ciclico normal, se puede pensar en colocar el canal de sincronizacion con CP Extendido 
+		#• Temporal structure:
+		#• 10 ms frame = 10 sub-frames of 1 ms
+		#• 1 sub-frame = 2 slots of 0.5 ms
 
-		#si piensas que esto es dificil, prueba computacion cuantica.
-		for indx, mapa in enumerate(range(1,self.max_usuario+1)):
-			arreglo=np.where(self.mapa_nrb==mapa)
-			self.lista_distribucion.append(arreglo[0])
-			##print("nrb",mapa, " repetidos:",arreglo[0])
-			mapa_semilla=[0 for i in range(len(self.mapa_conexion))]
-
-			for indxx in arreglo[0]:
-				#############################print(indxx, self.mapa_estado[indxx])
-				mapa_semilla=self.mapa_estado[indxx] + mapa_semilla
-			#print("------>semilla",mapa_semilla)
-			self.mapa_interferencia.append(mapa_semilla)
-		self.mapa_interferencia=np.stack(self.mapa_interferencia)
-		#self.lista_distribucion=np.stack(self.lista_distribucion)
-		##print("mapa interferencia\n",self.mapa_interferencia)
-		##print("mapa estados\n",self.mapa_estado)
-		#print("aux\n",self.mapa_interf_distribuida)
-		#print("mapa_distribucion\n", self.lista_distribucion)
-		for lista, mapa_dist in zip(self.lista_distribucion, self.mapa_interferencia):
-			#print(lista, mapa_dist)
-			for indx_interf in lista:
-				self.mapa_interf_distribuida[indx_interf]=mapa_dist
-				#print(indx_interf)
-		print("mapa de usuarios iterfentes\n",self.lista_distribucion)
-		#print("mapa de estados iterfentes\n",self.mapa_interferencia)
-		#print("mapa distribucion\n",self.mapa_interf_distribuida)
+		#• 1 slot = 7 symbols OFDM (6 symbols if extended CP) 
+		#numero de bloques de recursos salen de 38.104, banda de guarda 38.104
+		#Considerar la cantidad de bloques de recursos utilizados por el PBCH el cual se transmite cada 
+		#OFDM_CP="Extendido"
+		OFDM_CP='Normal'
+		bwmhz=100#Parametro fijo para el ancho de banda del sistema 
+		frportadoraGHz= 3.5
+		num=1
+		g_bw_khz=845
+		n_rb_sin_gbw=273
+		sub_ofdm=12
+		case_use_100mhz= ['URLLC','mMTC']
+		trama_tol=14
+		simb_ofdm_dl=10
+		frame=10
+		bw_con_gbw=(bwmhz*1000) - (2*g_bw_khz) 
+		bw_usuario=(2**num)*15*sub_ofdm
+		no_rb=bw_con_gbw/bw_usuario
+		rb_conPBCH= no_rb-22
+		
+		return no_rb, bw_usuario 
+		
 
 
-		#la matriz de potencia puede obtener [0 0 0 0 ... 0] en potencia.
-		#en este caso, no ha habido interferencia de otras celdas.
-
-
-
-
-
-
+	def asignar_200Mhz():
+		#asiganar ancho de banda en Megaherz y frecuencia portadora  con el fin de asignar la numerologia necesaria.
+		#Se tiene conciencia de que para cumplir con los 264 canales es necesario elevar la frecuencia portadora.
+		#el hecho es tener 264 canales maximos y eso se lleva acabo con tanto el espaciamiento entre portadoras.
+		#y la cantidad de subportadoras  (15KHz*2^numerologia)*12subportadoras = 720KHz Ahora el ancho  
+		OFDM_CP='Normal'
+		bwmhz=200#Parametro fijo para el ancho de banda del sistema 
+		frportadoraGHz= 24
+		num=2
+		g_bw_khz= 4930
+		n_rb_sin_gbw=264
+		sub_ofdm=12
+		case_use_200mhz= ['URLLC','mMTC','eBMM']
+		trama_tol=14
+		simb_ofdm_dl=10
+		frame=10
+		bw_con_gbw=(bwmhz*1000) - (2*g_bw_khz) 
+		bw_usuario=(2**num)*15*sub_ofdm
+		no_rb=bw_con_gbw/bw_usuario
+		
+		return no_rb, bw_usuario 
+	
+	def asignar_400mhz():
+		#
+		OFDM_CP='Normal'
+		bwmhz=400#Parametro fijo para el ancho de banda del sistema 
+		frportadoraGHz= 73
+		num=3
+		g_bw_khz= 9860
+		n_rb_sin_gbw=264
+		sub_ofdm=12
+		case_use_400mhz= ['URLLC','mMTC','eBMM']
+		trama_tol=14
+		simb_ofdm_dl=10
+		frame=10
+		bw_con_gbw=(bwmhz*1000) - (2*g_bw_khz) 
+		bw_usuario=(2**num)*15*sub_ofdm
+		no_rb=bw_con_gbw/bw_usuario
+		
+		return no_rb, bw_usuario 
 
 
 def prueba_asignar100():
@@ -163,11 +124,7 @@ def prueba_asignar100():
 	print(prueba2)
 	print("-----------------------------------------------Numero de bloques de recursos 400MHz ---------------")
 	print("-----------------------------------------------(Cantidad de bloques de recuros , ancho de banda por RB) ---------------")
-	print("test3",prueba3)
-
-def prueba_asignar():
-	"""Comprueba implementacion de funciones asignar 100,200,400 segun parametros"""
-	prueba=Planificador.asignar_100mhz()
+	print(prueba3)
 
 
 def lista_rb():
@@ -187,18 +144,19 @@ def lista_rb():
 	print("-----------------------------------------------(distribucion bloques de recursos por numero de usuario) ---------------")
 	print(m_rb)
 	#
+	
 
+	pass
 
 
 if __name__=="__main__":
 	#Prototipo:
 	print("planificador")
 
-	#plan=Planificador(params_cfg, 17)
+	#plan=Planificador(params, 17)
 	#Planificador.asignar_100mhz()
 	#REALIZAR PRUEBA DE F1,F2,F3
 	prueba_asignar100()
-	#lista_rb()
-	#prueba_asignar()
+	lista_rb()
 else:
 	print("Modulo Importado: [", os.path.basename(__file__), "]")
