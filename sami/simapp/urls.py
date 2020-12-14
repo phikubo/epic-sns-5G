21e6299a9697ee8919c0c8aef36a929dfd0f6113
phikubo <jmichaelten@gmail.com>
2020-12-14 14:31:40 -0500

    interfaces implementan base extend para mayor modularidad y rapidez

--- sami/simapp/urls.py
+++ sami/simapp/urls.py
@@ -6,7 +6,9 @@ app_name='simapp'
 urlpatterns = [
     #http://127.0.0.1:8000/ / 
     path('', views.home, name='caracteristicas'),
-    path('sami_v1/', views.formulario_sami_v1, name='sami_v1'),
+    path('sami_v1/', views.formulario_sami_v1, name='form_sami_v1'),
+    path('futuro/', views.en_desarrollo, name='futuro'),
+    #path('test/', views.test_sam, name='test'),
     
 
 ]
--- sami/simapp/views.py
+++ sami/simapp/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request,'simapp/index.html')
+    return render(request,'simapp/sami-index.html')
 
 def formulario_sami_v1(request):
     return render(request,'simapp/form-wizard.html')
+
+def en_desarrollo(request):
+    return render(request,'simapp/sami-en-desarrollo.html')
+
+#def test_sam(request):
+#    return render(request,'simapp/sami-index.html')
