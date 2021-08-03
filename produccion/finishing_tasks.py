27402134c4c4e8c0649c96c26de9943d7db88dc6
phikubo <jmichaelten@gmail.com>
2021-08-02 19:58:21 -0500

    last

---
+++ produccion/finishing_tasks.py
@@ -0,0 +1,18 @@
+#
+'''
+1. acuerdos con el profesor
+a. la antena debe ser unidireccional
+a.1 si es unidireccional que cambios debo hacer?
+    a.1.1 distribucion de recursos radios cambia
+    a.1.2 los angulos a los usuarios ya no es de importancia
+    a.1.3 que sucede con la sinr
+    a.1.3.1 la sinr en este caso debe disminuir? en que manera debe disminuir
+    a.1.3.2 la ganancia es maxima es igual a la ganancia de tx.
+2. verificar los calculos de througput
+    a. verificado tasa y modulacion, modulacion se vuelve positivo para que el tp se vuelva negativo cuando no contribuya
+        OK
+    b. data limpiada
+    c. info_sinr ahora es info_data
+
+3. mantener el tama;o de usuarios de acuerdo a la seleccion.
+'''
