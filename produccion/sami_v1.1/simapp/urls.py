d5f3d8c1eae1901edec6a08307ff4ba7c5d7c363
phikubo <jmichaelten@gmail.com>
2022-05-10 02:44:00 -0500

    Nueva pagina para comparar escenarios

--- produccion/sami_v1.1/simapp/urls.py
+++ produccion/sami_v1.1/simapp/urls.py
@@ -12,24 +12,30 @@ urlpatterns = [
     #path('sim/form_a3', views.form_a3, name='sim_form_a3'),
     #path('sim/form_a4', views.form_a4, name='sim_form_a4'),
     #
-    path('sim/seleccionar_escenario', views.seleccionar_escenario, name="sim_form_escenarios"),
+    path('simulador/parametros', views.ver_parametros, name='lista_parametros'),
     #
-    path('sim/form_compacto', views.form_compacto, name='sim_form_compacto'),
+    path('simulador/seleccionar_escenario', views.seleccionar_escenario, name="sim_form_escenarios"),
     #
-    path('sim/ejecutar_parametros/', views.ejecutar_parametros, name='parametros'),
+    path('simulador/form_compacto', views.form_compacto, name='sim_form_compacto'),
+    #
+    #SI TIENE REDIRECT, DEBE TERMINAR EN /, DE LO CONTRARIO NO ES NECESARIO.
+    #@redirect
+    path('simulador/ejecutar_parametros/', views.ejecutar_parametros, name='parametros'),
     #simulador
-    path('sim/iniciar/', views.iniciar_simulacion, name='iniciar_sim'),
+    path('simulador/iniciar', views.iniciar_simulacion, name='iniciar_sim'),
     #
-    path('sim/simulacion-general', views.ver_presim, name='pre_sim'),
-    path('sim/simulacion', views.ver_sim, name='sim'),
+    path('resultados/simulacion-general', views.ver_presim, name='pre_sim'),
+    path('resultados/simulacion', views.ver_sim, name='sim'),
     
     #path('simulador/tablas', views.ver_tablas, name='simulador_tablas'),
     #path('simulador/logs', views.ver_tablas, name='simulador_tablas'),
+    path('estadisticas/seleccionar_comparacion', views.seleccionar_comparacion, name="sim_form_comparar_escenarios"),
+    #@redirect
+    path('estadisticas/resultados_comparacion/', views.resultados_comparacion, name="sim_resultados_comparacion_escenarios"),
 
     #auxiliar
-    path('parametros/', views.ver_parametros, name='lista_parametros'),
-    path('demo_sami_v1/', views.form_demo_sami_v1, name='demo_form_sami_v1'),
-    path('futuro/', views.futuro, name='futuro'),
+    #path('extra/demo_sami_v1/', views.form_demo_sami_v1, name='demo_form_sami_v1'),
+    path('extra/futuro/', views.futuro, name='futuro'),
 
 
 ]
